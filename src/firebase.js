56c9fa4835b090dc550a0da7c13eeca4a7a541d5
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2023-07-31 11:17:48 +0330

    install firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0H-j6-Ge8WgZNLhkiVIllTpoTqH7V-DU",
+  authDomain: "shopping-redux-5ad57.firebaseapp.com",
+  projectId: "shopping-redux-5ad57",
+  storageBucket: "shopping-redux-5ad57.appspot.com",
+  messagingSenderId: "30493087475",
+  appId: "1:30493087475:web:bad4a4502f5269633cc986",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
