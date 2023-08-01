742c95c928e399570d4a2d5180d0339861d86f06
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2023-08-01 23:15:54 +0330

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
