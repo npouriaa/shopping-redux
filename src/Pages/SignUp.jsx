31b06e8a978e0666cf84a04444bd14dd039fb6b1
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2023-08-02 14:02:36 +0330

    validate the signUp form

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -7,16 +7,24 @@ import { Link } from "react-router-dom";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { useDispatch, useSelector } from "react-redux";
 import { actions } from "../redux/signUpSlice";
+import * as emailValidator from "email-validator";
 
 const SignUp = () => {
   const dispatch = useDispatch();
-  const { email, password, passwordVisibility, passwordError , emailError } = useSelector(
-    (state) => state.signUp
-  );
+  const { email, password, passwordVisibility, passwordError, emailError } =
+    useSelector((state) => state.signUp);
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(actions.setEmailError(email === "" ? true : false));
-    dispatch(actions.setPasswordError(password === "" ? true : false));
+    dispatch(
+      actions.setEmailError(
+        email === "" || !emailValidator.validate(email) ? true : false
+      )
+    );
+    dispatch(
+      actions.setPasswordError(
+        password.length < 6 || password === "" ? true : false
+      )
+    );
   };
 
   return (
@@ -28,8 +36,11 @@ const SignUp = () => {
       </div>
       <form onSubmit={(e) => handleSubmit(e)} className="form">
         <div className="input-con">
-          <AlternateEmailIcon htmlColor="#9a9a9a" />
+          <AlternateEmailIcon htmlColor={emailError ? "red" : "#9a9a9a"} />
           <TextField
+            helperText={
+              emailError ? "Plaese Enter a valid email address !" : ""
+            }
             onChange={(e) => dispatch(actions.setEmail(e.target.value))}
             className="input"
             size="small"
@@ -38,8 +49,9 @@ const SignUp = () => {
           />
         </div>
         <div className="input-con">
-          <HttpsIcon htmlColor="#9a9a9a" />
+          <HttpsIcon htmlColor={passwordError ? "red" : "#9a9a9a"} />
           <TextField
+            helperText={passwordError ? "Plaese Enter 6 char password !" : ""}
             onChange={(e) => dispatch(actions.setPassword(e.target.value))}
             type={passwordVisibility ? "text" : "password"}
             className="input"
