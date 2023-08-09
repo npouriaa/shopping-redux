0da8418d8eae3c568e9d493077101b398036cc17
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2023-08-09 20:01:31 +0330

    remove product from db

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "../scss/header.scss";
 import NavBar from "./NavBar";
-import SearchBar from "./SearchBar";
 
 const Header = () => {
   return (
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -13,8 +13,11 @@ import RemoveIcon from "@mui/icons-material/Remove";
 import { useDispatch, useSelector } from "react-redux";
 import { AuthContext } from "../Context/AuthContext";
 import { db } from "../firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc, deleteField } from "firebase/firestore";
 import { cartActions } from "../redux/cartSlice";
+import { notificationActions } from "../redux/notificationSlice";
+import Notification from "../Components/Notification";
+import { loaderActions } from "../redux/loaderSlice";
 
 const Cart = () => {
   const { cart } = useSelector((state) => state.cart);
@@ -34,57 +37,94 @@ const Cart = () => {
     }
   };
 
+  const deleteProduct = async (productTitle) => {
+    const documentRef = doc(db, "usersCarts", currentUser.uid);
+    dispatch(loaderActions.setLoading(true));
+    try {
+      const documentSnapshot = await getDoc(documentRef);
+
+      if (documentSnapshot.exists()) {
+        const data = documentSnapshot.data();
+        const modifiedArray = data?.cart.filter(
+          (item) => item.title !== productTitle
+        );
+
+        await updateDoc(documentRef, {
+          cart: modifiedArray,
+        });
+        dispatch(notificationActions.setOpen(true));
+        dispatch(notificationActions.setSeverity("success"));
+        dispatch(notificationActions.setMessage("Removed successfuly"));
+      }
+    } catch (err) {
+      dispatch(notificationActions.setOpen(true));
+      dispatch(notificationActions.setSeverity("error"));
+      dispatch(notificationActions.setMessage(err.message));
+    }
+    dispatch(loaderActions.setLoading(false));
+  };
+
+  const array = [];
+
   useEffect(() => {
     getUserCartProducts();
-  }, [cart]);
+    console.log(cart);
+  }, []);
 
   return (
     <div className="cart">
+      <Notification />
       <Header />
-      <TableContainer className="table-con">
-        <Table className="table" aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell className="table-header">Products</TableCell>
-              <TableCell className="table-header">Quantity</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {cart?.map((product) => (
-              <TableRow key={product.id} className="row">
-                <TableCell className="">
-                  <div className="product-con">
-                    <img src={product.imageSrc} alt="product-image" />
-                    <div className="text">
-                      <h3>{product.title}</h3>
-                      <h3>{product.price}</h3>
-                    </div>
-                  </div>
-                </TableCell>
-                <TableCell>
-                  <div className="quantity-con">
-                    <div className="counter-con">
-                      <button className="counter-btn">
-                        <RemoveIcon className="counter-icon" />
-                      </button>
-                      <h3>0</h3>
-                      <button className="counter-btn">
-                        <AddIcon className="counter-icon" />
-                      </button>
-                    </div>
-                    <button className="remove-btn">
-                      <DeleteIcon className="remove-icon" />
-                      Remove
-                    </button>
-                  </div>
-                </TableCell>
+      {cart.length > 0 ? (
+        <TableContainer className="table-con">
+          <Table className="table" aria-label="simple table">
+            <TableHead className="table-header-con">
+              <TableRow>
+                <TableCell className="table-header">Products</TableCell>
+                <TableCell className="table-header">Quantity</TableCell>
               </TableRow>
-            ))}
-            {/* <button onClick={() => console.log(cart)}>sasas</button> */}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <h2 className="total">Total Price : </h2>
+            </TableHead>
+            <TableBody className="tbody">
+              {cart.map((product) => (
+                <TableRow key={product.id} className="row">
+                  <TableCell className="">
+                    <div className="product-con">
+                      <img src={product.imageSrc} alt="product-image" />
+                      <div className="text">
+                        <h3>{product.title}</h3>
+                        <h3>{product.price}</h3>
+                      </div>
+                    </div>
+                  </TableCell>
+                  <TableCell>
+                    <div className="quantity-con">
+                      <div className="counter-con">
+                        <button className="counter-btn">
+                          <RemoveIcon className="counter-icon" />
+                        </button>
+                        <h3>0</h3>
+                        <button className="counter-btn">
+                          <AddIcon className="counter-icon" />
+                        </button>
+                      </div>
+                      <button
+                        onClick={() => deleteProduct(product.title)}
+                        className="remove-btn"
+                      >
+                        <DeleteIcon className="remove-icon" />
+                        Remove
+                      </button>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <h2 className="total">Total Price : </h2>
+        </TableContainer>
+      ) : (
+        <h4>Your cart is empty !</h4>
+      )}
     </div>
   );
 };
